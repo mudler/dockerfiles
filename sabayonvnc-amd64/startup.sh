dcf963cb2f6efdfb19f423a358e6dede1a88e585
mudler <mudler@sabayon.org>
2016-06-12 22:26:22 +0200

    [sabayonvnc-amd64] set root password

--- sabayonvnc-amd64/startup.sh
+++ sabayonvnc-amd64/startup.sh
@@ -7,6 +7,7 @@ PASS=sabayon
 # echo "Username: ubuntu Password: $PASS"
 id -u sabayon &>/dev/null || useradd --create-home --shell /bin/bash --user-group --groups wheel,video sabayon
 echo "sabayon:$PASS" | chpasswd
+echo "root:$PASS" | chpasswd
 [ -e /etc/sudoers ] && echo "sabayon ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 cd /web && ./run.py > /var/log/web.log 2>&1 &
