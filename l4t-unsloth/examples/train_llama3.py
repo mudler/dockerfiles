d6550a88d95907d3fcac33ea80cd1498f6151125
mudler <mudler@localai.io>
2025-02-01 18:51:57 +0100

    Add examples to the image

---
+++ l4t-unsloth/examples/train_llama3.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+import os
+
+hf_token = os.environ["HF_TOKEN"] 
+hf_repo = os.environ["HF_REPO"]
+dataset_names = os.environ["DATASETS"]
+
+model_name = os.environ.get("MODEL_NAME", "unsloth/Llama-3.2-3B-Instruct")
+
+chat_template = "llama-3.1"
+
+if not dataset_names:
+    dataset_names="mlabonne/FineTome-100k,mudler/glaive-unsloth-localai,mudler/o1-unsloth,mudler/open-o1-sft-unsloth,mudler/identity"
+    print(f"Using default datasets: {dataset_names}")
+
+if not hf_repo:
+    raise ValueError("Please set the HF_REPO environment variable to your Hugging Face repository name.")
+
+if not hf_token:
+    raise ValueError("Please set the HF_TOKEN environment variable to your Hugging Face API token.")
+
+from unsloth import FastLanguageModel  # FastVisionModel for LLMs
+import torch
+max_seq_length = 2048  # Choose any! We auto support RoPE Scaling internally!
+load_in_4bit = True  # Use 4bit quantization to reduce memory usage. Can be False.
+
+# 4bit pre quantized models we support for 4x faster downloading + no OOMs.
+fourbit_models = [
+    "unsloth/Meta-Llama-3.1-8B-bnb-4bit",  # Llama-3.1 2x faster
+    "unsloth/Mistral-Small-Instruct-2409",  # Mistral 22b 2x faster!
+    "unsloth/Phi-4",  # Phi-4 2x faster!
+    "unsloth/Phi-4-unsloth-bnb-4bit",  # Phi-4 Unsloth Dynamic 4-bit Quant
+    "unsloth/gemma-2-9b-bnb-4bit",  # Gemma 2x faster!
+    "unsloth/Qwen2.5-7B-Instruct-bnb-4bit"  # Qwen 2.5 2x faster!
+    "unsloth/Llama-3.2-1B-bnb-4bit",  # NEW! Llama 3.2 models
+    "unsloth/Llama-3.2-1B-Instruct-bnb-4bit",
+    "unsloth/Llama-3.2-3B-bnb-4bit",
+    "unsloth/Llama-3.2-3B-Instruct-bnb-4bit",
+]  # More models at https://docs.unsloth.ai/get-started/all-our-models
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = model_name,
+    max_seq_length = max_seq_length,
+    load_in_4bit = load_in_4bit,
+    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 16,
+    lora_dropout = 0, # Supports any, but = 0 is optimized
+    bias = "none",    # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+    random_state = 3407,
+    use_rslora = False,  # We support rank stabilized LoRA
+    loftq_config = None, # And LoftQ
+)
+
+from unsloth.chat_templates import get_chat_template
+
+tokenizer = get_chat_template(
+    tokenizer,
+    chat_template = chat_template,
+)
+
+def formatting_prompts_func(examples):
+    convos = examples["conversations"]
+    texts = [
+        tokenizer.apply_chat_template(
+            convo, tokenize = False, add_generation_prompt = False
+        )
+        for convo in convos
+    ]
+    return { "text" : texts, }
+
+from datasets import load_dataset
+from datasets import concatenate_datasets
+
+datasets = [load_dataset(name, split="train") for name in dataset_names.split(",")]
+
+print(f"Loaded {len(datasets)} datasets.")
+print(f"Datasets: {datasets}")
+
+# Combine datasets together
+dataset = concatenate_datasets(datasets)
+
+# Shuffle dataset (optional)
+dataset = dataset.shuffle(seed=0)
+
+from unsloth.chat_templates import standardize_sharegpt
+dataset = standardize_sharegpt(dataset)
+dataset = dataset.map(formatting_prompts_func, batched = True,)
+
+from trl import SFTTrainer
+from transformers import TrainingArguments, DataCollatorForSeq2Seq
+from unsloth import is_bfloat16_supported
+
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    data_collator = DataCollatorForSeq2Seq(tokenizer = tokenizer),
+    dataset_num_proc = 2,
+    packing = False, # Can make training 5x faster for short sequences.
+    args = TrainingArguments(
+        per_device_train_batch_size = 2,
+        gradient_accumulation_steps = 4,
+        warmup_steps = 5,
+        num_train_epochs = 1, # Set this for 1 full training run.
+        learning_rate = 2e-4,
+        fp16 = not is_bfloat16_supported(),
+        bf16 = is_bfloat16_supported(),
+        logging_steps = 1,
+        optim = "adamw_8bit",
+        weight_decay = 0.01,
+        lr_scheduler_type = "linear",
+        seed = 3407,
+        output_dir = "outputs",
+        report_to = "none", # Use this for WandB etc
+    ),
+)
+
+from unsloth.chat_templates import train_on_responses_only
+trainer = train_on_responses_only(
+    trainer,
+    instruction_part = "<|start_header_id|>user<|end_header_id|>\n\n",
+    response_part = "<|start_header_id|>assistant<|end_header_id|>\n\n",
+)
+
+trainer_stats = trainer.train()
+
+
+model.push_to_hub_merged(hf_repo, tokenizer, save_method = "merged_16bit", token = hf_token)
